5aff6e22c26b64375efa837d3663145ced94af7e
AndresSern <andrecamposer@gmail.com>
2021-01-06 20:48:23 +0000

    Declarating Variables using let

---
+++ fundamentals/values_Variables.js
@@ -0,0 +1,14 @@
+let js = "amazing"; //Declaring variable
+let firstName = "Matilda"; //Declaring Variable
+console.log(firstName); // Print in the console.log the variable firstName
+console.log(40 + 8 + 23 - 10); //Print Numbers with console.log
+
+// let 5jonas = 4;  This is ilegal beacuse the variable begins with a number
+// let jonas&andrs = "hola"; This is ilegal beacuse the variable has the character "&"
+
+let myFirstJob = 'Programmer'; // Descriptive Variables
+let myCurrentJob = 'Teacher';  //Descriptive Variables
+
+let job1 = 'Programmer'; // Non-descriptive variables
+let job2  = 'Teacher'; // Non-descriptive variables
+
