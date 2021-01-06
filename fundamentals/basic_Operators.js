fa504ef706d020ad943a6b84ae9f2ff61d122a06
AndresSern <andrecamposer@gmail.com>
2021-01-06 20:47:35 +0000

    Adding basic operators, sum, sub, and mult

---
+++ fundamentals/basic_Operators.js
@@ -0,0 +1,21 @@
+const firstName = 'Jonas';
+const lastName = 'Campo';
+console.log(firstName + ' ' + lastName);
+
+// Assigment Operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--; // x = x - 1
+console.log(x); // Print 99
+
+// Comparison Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(now - 1991 > now - 2018);
+let a, b;
+a = b = 25 - 10 - 5;
+console.log(a, b);
