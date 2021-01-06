ad99e798086b8f2a2a4488c8252022be510e134c
AndresSern <andrecamposer@gmail.com>
2021-01-06 20:50:01 +0000

    Declarating data type with let, usign boolean number, and string

---
+++ fundamentals/data_Type.js
@@ -0,0 +1,15 @@
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true); //This data type is a boolean
+console.log(javascriptIsFun); //This data type is a boolean
+console.log(typeof 23); //This data type is a number
+console.log(typeof 'Jonas'); //This data type is a string
+
+javascriptIsFun = "Yes"; // Changed of boolean to a string
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year); //Cann't display nothig because is undefined
+console.log(typeof year); //This data type is undefined
+console.log(typeof null); //Null is data type object
