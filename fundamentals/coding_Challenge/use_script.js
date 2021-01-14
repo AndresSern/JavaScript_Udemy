ff2719af553136d2d2dfb330c690fa28ebde7ce9
AndresSern <andrecamposer@gmail.com>
2021-01-14 03:25:51 +0000

    Adding file use_strict.js

---
+++ fundamentals/coding_Challenge/use_script.js
@@ -0,0 +1,8 @@
+'use strict';
+let hasDriverslicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; //This is an error an use strict beacuse, we have to declare the variable with const, let or var
+if (hasDriversLicense) console.log('I can Drive :D');
+
+const interface = 'Audio';
---
+++ fundamentals/coding_Challenge/use_strict.js
@@ -0,0 +1,9 @@
+'use strict';
+let hasDriverslicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; //This is an error an use strict beacuse, we have to declare the variable with const, let or var
+if (hasDriverslicense) console.log('I can Drive :D');
+
+const interface = 'Audio'; // Unexpected strict mode reserved Word
+const private = 534;
